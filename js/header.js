c9d2db005f0659c9f3a4a62572ae83e67df3ba61
sheharyark017 <sheharyark017@gmail.com>
2021-12-23 21:29:23 +0500

    first commit

---
+++ js/header.js
@@ -0,0 +1,19 @@
+const nav = document.querySelector(".navbar");
+
+const topSection = document.querySelector(".top-section");
+
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  // console.log(entry);
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+};
+
+const headObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headObserver.observe(topSection);
